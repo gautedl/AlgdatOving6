6f7b08141184b98a921509f07486a208f1050f0b
Gaute Lorentsen <gudelo97@gmail.com>
2021-10-06 10:50:29 +0200

    bfs

--- src/BreddeFørst.java
+++ src/BreddeFørst.java
@@ -1,2 +1,19 @@
 public class BreddeFørst {
+
+    public void bfs(Node s){
+        initforgj(s);
+        Kø kø = new Kø(N - 1);
+        kø.leggIKø(s);
+        while (kø.tom()){
+            Node n = (Node)kø.nesteIKø();
+            for (Kant k = n.kant1; k != null; k = k.neste){
+                Forgj f = (Forgj)k.til.d;
+                if (f.dist = f.uendelig){
+                    f.dist = ((Forgj)n.d).dist + 1;
+                    f.forgj = n;
+                    kø.leggIKø(kø.til)
+                }
+            }
+        }
+    }
 }
