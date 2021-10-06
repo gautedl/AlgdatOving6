80023130d69824b32c017a2e5352c767f916661c
KingShaggy <39702459+KingShaggy@users.noreply.github.com>
2021-10-06 12:23:00 +0200

    oops

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        public void ny_ugraftab(BufferedReader br) throws IOException {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            Kanttab[][] kant = new Kanttab[N][N];
-            for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) kant[i][j] = new Kanttab();
-            int K = Integer.parseInt(st.nextToken());
-            for (int i=0; i<K; i++) {
-                st = new StringTokenizer(br.readLine());
-                int fra = Integer.parseInt(st.nextToken());
-                int til = Integer.parseInt(st.nextToken());
-                kant[fra][til].fins=1;
-            }
+    public void ny_ugraftab(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        Kanttab[][] kant = new Kanttab[N][N];
+        for (int i=0; i<N; ++i) for (int j=0; j<N; ++j) kant[i][j] = new Kanttab();
+        int K = Integer.parseInt(st.nextToken());
+        for (int i=0; i<K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int fra = Integer.parseInt(st.nextToken());
+            int til = Integer.parseInt(st.nextToken());
+            kant[fra][til].fins=1;
         }
+    }
+    
+    public static void main(String[] args) {
     }
 }
